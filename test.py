8628013e7ddc7ba47ce8bf259a757ba8803f456b
Shivendra <shivharsh44@gmail.com>
2025-01-19 00:42:04 +0530

    added a readme file, trained a tokenizer & saved the vocab

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 from shredword import Shred
 
 tokenizer = Shred()
-input_file = "test data/captions.txt"
-train_file = "test data/new.txt"
+input_file = "test data/training_data.txt"
+# train_file = "test data/new.txt"
 
 with open(input_file, "r", encoding="utf-8") as f:
   text = f.read()
-with open(train_file, "r", encoding="utf-8") as f:
-  train = f.read()
+# with open(train_file, "r", encoding="utf-8") as f:
+#   train = f.read()
 
-VOCAB_SIZE = 556
-tokenizer.train(text, VOCAB_SIZE)
-tokenizer.save("vocab/trained_vocab")
+# VOCAB_SIZE = 556
+# tokenizer.train(text, VOCAB_SIZE)
+# tokenizer.save("vocab/trained_vocab")
 tokenizer.load("vocab/trained_vocab.model")
 
-encoded = tokenizer.encode(train)
+encoded = tokenizer.encode(text)
 print("Encoded:", encoded)
 
 decoded = tokenizer.decode(encoded)
