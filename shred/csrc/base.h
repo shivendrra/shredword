7458d6c6dafe57633c64e06c19b5f62d66ffaa78
Shivendra <shivharsh44@gmail.com>
2025-05-15 23:28:24 +0530

    added `trie` & related functions

--- shred/csrc/base.h
+++ shred/csrc/base.h
@@ -8,26 +8,14 @@
 #ifndef __BASE__H__
 #define __BASE__H__
 
-#define NUM_CHARS 256   // Maximum initial letters, since UTF-8 so 256
+#include "trie.h"
+
 #define MAX_LINE_LENGTH 1024  // Max length of line to be read
 #define MAX_TOKENS 1000000   // Max tokens to be trained
 #define MAX_SEQ_LENGTH 4096   // BUffer length for loading sentences
 #define MIN_SYMBOL_LEN 32   // Symbols size (to be increased dynamically)
 
-typedef struct TrieNode {
-  struct TrieNode *children[NUM_CHARS];
-  bool terminal;
-} TrieNode;   // Trie-based dtype to store possible vocabs
-
 extern "C" {
-  // functions for creating/modifying `trie`
-  TrieNode* create_node();
-  void trie_insert(TrieNode* root, const char* word);
-  int longest_prefix(TrieNode* root, const char* text);
-  int trie_count_words(TrieNode* node);
-  void free_trie(TrieNode* node);   // freeing the trie from the memory
-  void print_trie(TrieNode* node);  // prints all the nodes recursively
-
   int split_to_symbols(const char* line, char*** out_symbols);
   void load_and_split(const char* train_file, char**** out_symbols, int** out_lens, int* out_size);
 
---
+++ shred/csrc/trie.h
@@ -0,0 +1,26 @@
+/**
+  @brief trie-based dtype to store possible vocabs
+  
+  * trie-based functions for creating, inserting & deleting tries & entries
+  * word-count & printing functions
+*/
+
+#ifndef __TRIE__H__
+#define __TRIE__H__
+
+#define  NUM_CHARS  256   // Maximum initial letters, since UTF-8 so 0-255
+
+typedef struct TrieNode {
+  struct TrieNode* children[256];
+  bool terminal;
+} TrieNode;   // Trie-based dtype to store possible vocabs
+
+extern "C" {
+  TrieNode* create_node();
+  void trie_insert(TrieNode* root, const char* word);
+  int longest_prefix(TrieNode* root, const char* text);
+  int trie_count_words(TrieNode* node);   // freeing the trie from the memory
+  void print_trie(TrieNode* node);   // prints all the nodes recursively
+}
+
+#endif
