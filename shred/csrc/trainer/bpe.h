719e20a3508c3bd7924355c35e88bd86c31e10a6
Shivendra <shivharsh44@gmail.com>
2025-05-23 18:19:02 +0530

    building trainer's python interface

--- shred/csrc/trainer/bpe.h
+++ shred/csrc/trainer/bpe.h
@@ -1,3 +1,13 @@
+/** 
+  @brief bpe.h header file for the bpr trainer code logic.
+  * each new vocab is determined & merged based on traditional bpe merging
+      with help of hashing & heaps for faster merges.
+  * main entry point file code for BPE-trainer related codebase.
+  * compile it as:
+    *- '.so': g++ -shared -fPIC -o libtrainer.so bpe.cpp hash.cpp heap.cpp
+    *- '.dll': g++ -shared -o libtrainer.dll bpe.cpp hash.cpp heap.cpp
+*/
+
 #ifndef __BPE__H__
 #define __BPE__H__
 
